95e045fbc2be339ab891c5df753f4ee46cb2412c
muddassirali8089 <114619571+muddassirali8089@users.noreply.github.com>
2025-07-14 23:44:11 +0500

    add the tour to the server
    add the tour to the server

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { json } from "express";
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from 'url';
@@ -7,12 +7,14 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+
 const port = 8000;
 const app = express();
+app.use(express.json());
 
 // Read tours data
 const tours = JSON.parse(
-  fs.readFileSync(path.join(__dirname, 'dev-data/data/tours.json'))
+  fs.readFileSync(path.join(__dirname,'dev-data/data/tours-simple.json'))
 );
 
 app.get("/api/v1/tours", (req, res) => {
@@ -24,6 +26,29 @@ app.get("/api/v1/tours", (req, res) => {
     }
   });
 });
+
+
+app.post("/api/v1/tours" , (req , res) => {
+
+    const newId = tours[tours.length-1].id + 1;
+    const newTour = Object.assign({id:newId} , req.body);
+
+    tours.push(newTour);
+
+    fs.writeFile(path.join(__dirname,'dev-data/data/tours-simple.json') , JSON.stringify(tours) , (err) => {
+
+        res.status(201).json({
+            status : "success",
+            data : {
+                tour: newTour
+            }
+        })
+    })
+
+
+  
+
+})
 
 app.listen(port, () => {
   console.log(`server is running on the port ${port}`);
