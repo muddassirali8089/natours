820c4ea434f485b73e988a729c572f52a1c69864
muddassirali8089 <114619571+muddassirali8089@users.noreply.github.com>
2025-07-18 03:41:09 +0500

    delete and update the tour

--- controllers/tour.controller.js
+++ controllers/tour.controller.js
@@ -100,7 +100,25 @@ export const getTour = async (req, res) => {
   }
 };
 
-export const updateTour = (req, res) => {
+export const updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        tour
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
   // const id = req.params.id * 1;
   // if (id > tours.length) {
   //   return res.status(404).json({
@@ -116,9 +134,27 @@ export const updateTour = (req, res) => {
   // });
 };
 
-export const deleteTour = (req, res) => {
-  // res.status(204).json({
-  //   status: "success",
-  //   data: null,
-  // });
+export const deleteTour = async (req, res) => {
+
+
+
+  try {
+    
+
+    await Tour.findByIdAndDelete(req.params.id)
+
+    res.status(204).json({
+    status: "success",
+    data: null,
+  });
+
+  } catch (err) {
+
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+    
+  }
+ 
 };
